046a060b59e5acce508b0d113b77dd3ad412063b
Mohamed <demahom93@yahoo.co.uk>
2023-10-04 12:48:25 +0300

    updates to the methods

--- src/app/records/students/students.ts
+++ src/app/records/students/students.ts
@@ -18,6 +18,7 @@ import { Stream } from 'stream';
         ]),
     ],
 })
+
 export class StudentsComponent implements OnInit{
     studentsList: any = []
     params!: FormGroup;
@@ -75,17 +76,39 @@ export class StudentsComponent implements OnInit{
     // });
 
     // }
-    retrieveStudents(): void {
-        this.studentService.getAll()
-        .subscribe({
-            next: (data) => {
-            this.studentsList = data;
-            console.log(data);
-        },
-        error:(e) => console.error(e)
-        });
 
+
+    retrieveStudents(): void {
+        // Show a loading indicator
+        this.isLoading = true;
+
+        this.studentService.getAll().subscribe({
+            next: (data) => {
+                // Update the studentsList
+                this.studentsList = data;
+                console.log('Students data retrieved successfully:', data);
+            },
+            error: (error) => {
+                console.error('Error retrieving students:', error);
+                // Handle the error and show an error message to the user
+                this.showMessage('An error occurred while retrieving students.');
+            },
+            complete: () => {
+                // Hide the loading indicator
+                this.isLoading = false;
+            }
+        });
     }
+
+
+
+
+
+// deleteStudent(student: any = null) {
+    //     this.studentsList = this.studentsList.filter((d: any) => d.id != student.id);
+    //     this.searchStudents();
+    //     this.showMessage('Students records have been deleted successfully.');
+    // }
 
 
     searchStudents() {
@@ -124,87 +147,125 @@ export class StudentsComponent implements OnInit{
             this.showMessage('Date of Birth is required.', 'error');
             return;
         }
-        if (this.params.controls['gender'].errors) {
-        this.showMessage('Please Select an option.', 'error');
-            return;
-        }
-        if (this.params.controls['disability'].errors) {
-        this.showMessage('Please Select a valid option', 'error');
-            return;
-        }
-        if (this.params.controls['school'].errors) {
-            this.showMessage('Please Select School', 'error');
-            return;
-        }
-        if (this.params.controls['grade'].errors) {
-        this.showMessage('Please input students grade', 'error');
-            return;
-        }
-        if (this.params.controls['stream'].errors) {
-        this.showMessage('Please input students stream.', 'error');
-            return;
-        }
-        if (this.params.controls['date_joined'].errors) {
-            this.showMessage('Date Joined is required.', 'error');
-            return;
-        }
+        // if (this.params.controls['gender'].errors) {
+            // this.showMessage('Please Select an option.', 'error');
+            //     return;
+            // }
+            // if (this.params.controls['disability'].errors) {
+                // this.showMessage('Please Select a valid option', 'error');
+                //     return;
+                // }
+                // if (this.params.controls['school'].errors) {
+                    //     this.showMessage('Please Select School', 'error');
+                    //     return;
+                    // }
+                    if (this.params.controls['grade'].errors) {
+                        this.showMessage('Please input students grade', 'error');
+                        return;
+                    }
+                    if (this.params.controls['stream'].errors) {
+                        this.showMessage('Please input students stream.', 'error');
+                        return;
+                    }
+                    if (this.params.controls['date_joined'].errors) {
+                        this.showMessage('Date Joined is required.', 'error');
+                        return;
+                    }
 
 
 
-        if (this.params.value.id) {
-            // update student
-            let student: any = this.studentsList.find((d: any) => d.id === this.params.value.id);
-            student.first_name = this.params.value.first_name;
-            student.last_name = this.params.value.last_name;
-            student.uid = this.params.value.uid;
-            student.school = this.params.value.school;
-            student.grade = this.params.value.grade;
-            student.date_joined = this.params.value.date_joined;
-        } else {
-            //add student
-            let maxStudentId = this.studentsList.length
-                ? this.studentsList.reduce((max: any, character: any) => (character.id > max ? character.id : max), this.studentsList[0].id)
-                : 0;
+                    if (this.params.value.id) {
+                        // update student
+                        let student: any = this.studentsList.find((d: any) => d.id === this.params.value.id);
+                        student.first_name = this.params.value.first_name;
+                        student.last_name = this.params.value.last_name;
+                        student.dob = this.params.value.dob;
+                        student.gender = this.params.value.gender;
+                        student.disability = this.params.value.disability;
+                        student.school = this.params.value.school;
+                        student.grade = this.params.value.grade;
+                        student.stream = this.params.value.stream;
+                        student.date_joined = this.params.value.date_joined;
+                    } else {
+                        //add student
+                        let maxStudentId = this.studentsList.length
+                        ? this.studentsList.reduce((max: any, character: any) => (character.id > max ? character.id : max), this.studentsList[0].id)
+                        : 0;
 
-            let student = {
-                id: maxStudentId + 1,
-                first_name : this.params.value.name,
-                last_name : this.params.value.name,
-                dob: this.params.value.dob,
-                gender: this.params.value.gender,
-                disability: this.params.value.disability,
-                school : this.params.value.school,
-                grade : this.params.value.grade,
-                stream: this.params.value.stream,
-                date_joined : this.params.value.date_joined,
-            };
-            this.studentsList.splice(0, 0, student);
-            this.searchStudents();
-        }
+                        let student = {
+                            id: maxStudentId + 1,
+                            first_name : this.params.value.first_name,
+                            last_name : this.params.value.last_name,
+                            dob: this.params.value.dob,
+                            gender: this.params.value.gender,
+                            disability: this.params.value.disability,
+                            school : this.params.value.school,
+                            grade : this.params.value.grade,
+                            stream: this.params.value.stream,
+                            date_joined : this.params.value.date_joined,
+                        };
 
-        this.showMessage('Student has been saved successfully.');
-        this.addStudentModal.close();
-    }
+                        this.studentService.create(student)
+                        .subscribe({
+                            next: (res) => {
+                                console.log(res);
+                            },
+                            error: (e) =>console.error(e)
+
+                        })
+
+                    }
+
+                    this.showMessage('Student has been saved successfully.');
+                    this.addStudentModal.close();
+                }
+
+                // saveStudent() {
+                    //     this.studentService.create(this.initForm.params);
+                    //     .subscribe({
+                        //         next: (res) => {
+                            //         console.log(res);
+                            //     })
+                            // }
 
 
-    deleteStudent(student: any = null) {
-        this.studentsList = this.studentsList.filter((d: any) => d.id != student.id);
-        this.searchStudents();
-        this.showMessage('Students records have been deleted successfully.');
-    }
 
-    showMessage(msg = '', type = 'success') {
-        const toast: any = Swal.mixin({
-            toast: true,
-            position: 'top',
-            showConfirmButton: false,
-            timer: 3000,
-            customClass: { container: 'toast' },
-        });
+
+                            showMessage(msg = '', type = 'success') {
+                                const toast: any = Swal.mixin({
+                                    toast: true,
+                                    position: 'top',
+                                    showConfirmButton: false,
+                                    timer: 3000,
+                                    customClass: { container: 'toast' },
+                                });
         toast.fire({
             icon: type,
             title: msg,
             padding: '10px 20px',
         });
     }
+
+deleteStudent(id: number) {
+    this.studentService.delete(id).subscribe({
+        next: () => {
+            this.studentsList = this.studentsList.filter((d: any) => d.id !== id);
+            console.log('Student details have been deleted successfully');
+            this.showMessage('Student record has been deleted successfully.');
+            this.retrieveStudents(); // Assuming this updates the list from the server
+        },
+        error: (error) => {
+            console.error('Error deleting student:', error);
+            // Handle the error, show an error message, etc.
+            // You can also revert any changes made to the studentsList.
+            this.showMessage('An error occurred while deleting the student.');
+        }
+    });
+}
+
+
+
+
+
+
 }
--- src/app/service/student.service.ts
+++ src/app/service/student.service.ts
@@ -26,11 +26,11 @@ export class StudentService {
       }
 
     update(id: any, data: any): Observable<any> {
-        return this.http.put('${baseUrl}/${id}', data);
+        return this.http.put('${baseUrl}/${id}/', data);
     }
 
     delete(id: any): Observable<any> {
-        return this.http.delete('${baseUrl}/${id}');
+        return this.http.delete('/${baseUrl}/${id}/');
     }
 
  }
